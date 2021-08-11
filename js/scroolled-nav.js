fddd44483f870479feb52bef7a8fa77867bee759
abelcg <abelcg86@gmail.com>
2021-08-11 04:49:34 -0300

    script de tipeado en banner principal

--- js/scroolled-nav.js
+++ js/scroolled-nav.js
@@ -1,5 +1,13 @@
+/* script que cambia el color del navbar al scrollear */
+
 $(window).on("scroll", function(){
     $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
+}); 
+
+/*  script que hace aparecer el botón up al scrollear */
+
+$(window).on("scroll", function(){
+    $("#up-btn").toggleClass("show", $(this).scrollTop() > 700);
 });  
 
 
---
+++ js/types.js
@@ -0,0 +1,8 @@
+/*  script que simula el tipiado de alfuna frase */
+
+var typed = new Typed(".typing", {
+    strings: ["Fullstack Web Developer", "Ingeniero Biomédico", "Freelancer"],
+    typeSpeed: 120,
+    backSpeed: 70,
+    loop: true
+});
